ebd2abd88226b2cd6930922ba40535942ea05cc0
Wijdan Cederlid <wijdan.cederlid@iths.se>
2022-12-22 14:39:31 +0100

    Added normalizing to my script

--- normalizing.sql
+++ normalizing.sql
@@ -1,7 +1,6 @@
 USE iths;
 
 DROP TABLE IF EXISTS UNF;
-
 CREATE TABLE `UNF` (
     `Id` DECIMAL(38, 0) NOT NULL,
     `Name` VARCHAR(26) NOT NULL,
@@ -14,7 +13,6 @@ CREATE TABLE `UNF` (
     `MobilePhone1` VARCHAR(15),
     `MobilePhone2` VARCHAR(15)
 )  ENGINE=INNODB;
-
 LOAD DATA INFILE '/var/lib/mysql-files/denormalized-data.csv'
 INTO TABLE UNF
 CHARACTER SET latin1
@@ -23,16 +21,34 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DROP TABLE IF EXISTS Student;
 
+
+DROP TABLE IF EXISTS Student;
 CREATE TABLE Student (
-    Id INT NOT NULL,
+    StudentId INT NOT NULL,
     FirstName VARCHAR(255) NOT NULL,
     LastName VARCHAR(255) NOT NULL,
-    CONSTRAINT PRIMARY KEY (Id)
+    CONSTRAINT PRIMARY KEY (StudentId)
 )  ENGINE=INNODB;
-
-INSERT INTO Student (ID, FirstName, LastName) 
+INSERT INTO Student (StudentId, FirstName, LastName) 
 SELECT DISTINCT Id, SUBSTRING_INDEX(Name, ' ', 1), SUBSTRING_INDEX(Name, ' ', -1) 
 FROM UNF;
+
+
+
+DROP TABLE IF EXISTS School;
+CREATE TABLE School AS SELECT DISTINCT 0 AS SchoolId, School AS Name, City FROM UNF;
+SET @id = 0;
+UPDATE School SET SchoolId = (SELECT @id := @id + 1);
+ALTER TABLE School ADD PRIMARY KEY(SchoolId);
+
+
+CREATE TABLE StudentSchool AS SELECT DISTINCT UNF.Id AS StudentId, School.SchoolId
+FROM UNF INNER JOIN School ON UNF.School = School.Name;
+ALTER TABLE StudentSchool MODIFY COLUMN StudentId INT;
+ALTER TABLE StudentSchool MODIFY COLUMN SchoolId INT;
+ALTER TABLE StudentSchool ADD PRIMARY KEY(StudentId, SchoolId);
+
+
+
 
